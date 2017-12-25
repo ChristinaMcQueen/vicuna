3be046e9dd75c07fab7eb3b9c6b08a705a3afee9
秦铭 <qinming@Christina-Mac.local>
2017-12-25 11:10:02 +0800

    modify

---
+++ src/animations/main.js
@@ -0,0 +1,6 @@
+import init from 'common/bootstrap';
+
+import router from './router/index';
+import store from './store/index';
+
+export default init({ router, store });
---
+++ src/animations/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    // modules: {
+    //    PatientList
+    // },
+    // strict: debug,
+    // plugins: [storeCache(sessionStorage, name)],
+    // mutations: {}
+});
